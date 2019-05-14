aa1d0a71243eaba0eb8485ad83406db83168d6a9
Roman Palaznik <rpalaznik.c@mesosphere.io>
2019-05-14 18:26:59 +0300

    DCOS-53756: Unit tests for StatsD reporter - Switches from JUnit3 to JUnit4 - Adds unit tests for MetricFormatter

---
+++ spark-statsd-reporter/src/test/java/org/apache/spark/metrics/sink/statsd/MetricFormatterTest.java
@@ -0,0 +1,72 @@
+package org.apache.spark.metrics.sink.statsd;
+
+import org.junit.Test;
+
+import static org.apache.spark.metrics.sink.statsd.MetricType.*;
+import static org.junit.Assert.assertTrue;
+
+
+public class MetricFormatterTest {
+
+    @Test
+    public void simpleDriverMetricString() {
+        InstanceDetailsProvider instanceDetailsProviderMock = new InstanceDetailsProviderMock(
+                "834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001", "Test Spark App",
+                InstanceType.DRIVER, "aa31a823-2c7c-40e0-aef9-4b2a42adb461", "default");
+        String[] tags = { };
+        MetricFormatter formatter = new MetricFormatter(instanceDetailsProviderMock, "spark", tags);
+
+        String actual = formatter.buildMetricString("834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001.driver.TestSource", "test_metric", 1, GAUGE);
+
+        assertTrue(actual.startsWith("spark.driver.TestSource.test_metric,"));
+        assertTrue(actual.contains(",spark_app_name=Test_Spark_App"));
+        assertTrue(actual.contains(",spark_instance=driver"));
+        assertTrue(actual.contains(",spark_instance_id=aa31a823-2c7c-40e0-aef9-4b2a42adb461"));
+        assertTrue(actual.contains(",spark_namespace=default"));
+        assertTrue(actual.endsWith(":1|g"));
+    }
+
+    @Test
+    public void simpleExecutorMetricString() {
+        InstanceDetailsProvider instanceDetailsProviderMock = new InstanceDetailsProviderMock(
+                "834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001", "Test Spark App",
+                InstanceType.EXECUTOR, "aa31a823-2c7c-40e0-aef9-4b2a42adb461_0", "default");
+        String[] tags = { };
+        MetricFormatter formatter = new MetricFormatter(instanceDetailsProviderMock, "spark", tags);
+
+        String actual = formatter.buildMetricString("834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001.aa31a823-2c7c-40e0-aef9-4b2a42adb461_0.TestSource", "test_metric", 1, GAUGE);
+
+        assertTrue(actual.startsWith("spark.executor.TestSource.test_metric,"));
+        assertTrue(actual.contains(",spark_app_name=Test_Spark_App"));
+        assertTrue(actual.contains(",spark_instance=executor"));
+        assertTrue(actual.contains(",spark_instance_id=aa31a823-2c7c-40e0-aef9-4b2a42adb461_0"));
+        assertTrue(actual.contains(",spark_namespace=default"));
+        assertTrue(actual.endsWith(":1|g"));
+    }
+
+    @Test
+    public void predefinedTags() {
+        InstanceDetailsProvider instanceDetailsProviderMock = new InstanceDetailsProviderMock(
+                "834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001", "Test Spark App",
+                InstanceType.DRIVER, "aa31a823-2c7c-40e0-aef9-4b2a42adb461", "default");
+        String[] tags = { "foo=bar" };
+        MetricFormatter formatter = new MetricFormatter(instanceDetailsProviderMock, "spark", tags);
+
+        String actual = formatter.buildMetricString("834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001.driver.TestSource", "test_metric", 1, GAUGE);
+
+        assertTrue(actual.contains(",foo=bar"));
+    }
+
+    @Test
+    public void precisionFormat() {
+        InstanceDetailsProvider instanceDetailsProviderMock = new InstanceDetailsProviderMock(
+                "834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001", "Test Spark App",
+                InstanceType.DRIVER, "aa31a823-2c7c-40e0-aef9-4b2a42adb461", "default");
+        String[] tags = { };
+        MetricFormatter formatter = new MetricFormatter(instanceDetailsProviderMock, "spark", tags);
+
+        String actual = formatter.buildMetricString("834d77b5-a7b1-4c9d-9742-0f95d39d15e0-0002-driver-20190430095449-0001.driver.TestSource", "test_metric", 1.1234, GAUGE);
+
+        assertTrue(actual.endsWith(":1.12|g"));
+    }
+}
---
+++ spark-statsd-reporter/src/test/java/org/apache/spark/metrics/sink/statsd/StatsdReporterTest.java
@@ -0,0 +1,13 @@
+package org.apache.spark.metrics.sink.statsd;
+
+
+import org.junit.Test;
+
+public class StatsdReporterTest {
+
+    @Test
+    public void testSomething() {
+
+    }
+
+}
